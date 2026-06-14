c1436793a426b559e3b5434333ff72c80bc025a7
kskuleshova <kuleshovaa.oksana@gmail.com>
2026-06-14 12:03:43 +0300

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,11 @@ import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 export default defineConfig({
   tanstackStart: {
     server: { entry: "server" },
-    ssr: {
-      external: ["openai", "pdfjs-dist"]
-    }
   },
   nitro: {
     preset: "vercel",
+  },
+  ssr: {
+    noExternal: ["openai", "pdfjs-dist"]
   }
 });
